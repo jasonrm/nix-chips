e04523a88962ab310be82ca4242d086425cbd0a4
Jason R. McNeil <jason@mcneil.dev>
2021-11-21 15:31:20 -0700

    go: init

---
+++ modules/programs/go.nix
@@ -0,0 +1,29 @@
+{ system, pkgs, lib, config, ... }:
+with lib;
+let
+  cfg = config.programs.go;
+in
+{
+  options = with lib.types; {
+    programs.go = {
+      enable = mkEnableOption "go support";
+
+      pkg = mkOption {
+        type = package;
+        default = pkgs.go;
+      };
+    };
+  };
+
+  config = {
+    shell = mkIf cfg.enable {
+      contents = [
+        cfg.pkg
+      ];
+    };
+
+    outputs.apps.go = {
+      program = "${cfg.pkg}/bin/go";
+    };
+  };
+}
