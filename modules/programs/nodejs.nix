cca8623e10ee9b306703dc6ac2376d0857082f58
Jason R. McNeil <jason@mcneil.dev>
2021-11-21 15:31:15 -0700

    nodejs: init

---
+++ modules/programs/nodejs.nix
@@ -0,0 +1,38 @@
+{ system, pkgs, lib, config, ... }:
+with lib;
+let
+  inherit (pkgs.writers) writeBashBin;
+
+  cfg = config.nodejs;
+in
+{
+  options = with lib.types; {
+    nodejs = {
+      enable = mkEnableOption "nodejs support";
+
+      pkg = mkOption {
+        type = package;
+        default = pkgs.nodejs;
+      };
+    };
+  };
+
+  config = {
+    shell = mkIf cfg.enable {
+      shellHooks = [
+        ''echo node: ${cfg.pkg}/bin/node''
+      ];
+      contents = [
+        cfg.pkg
+      ];
+    };
+
+    outputs.apps.node = {
+      program = "${cfg.pkg}/bin/node";
+    };
+
+    outputs.apps.npm = {
+      program = "${cfg.pkg}/bin/npm";
+    };
+  };
+}
