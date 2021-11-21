460e6fc943269270ca86997d1f41462bb53e0b6d
Jason R. McNeil <jason@mcneil.dev>
2021-11-21 15:31:58 -0700

    dir: init

---
+++ modules/config/dir.nix
@@ -0,0 +1,50 @@
+{ lib, pkgs, config, ... }:
+let
+  inherit (lib) mkOption;
+  cfg = config.dir;
+in
+{
+  imports = [
+  ];
+
+  options = with lib.types; {
+    dir = {
+      root = mkOption {
+        type = str;
+        default = "/usr/local";
+      };
+
+      log = mkOption {
+        type = str;
+        readOnly = true;
+        default = "${cfg.root}/var/log";
+      };
+
+      run = mkOption {
+        type = str;
+        readOnly = true;
+        default = "${cfg.root}/var/run";
+      };
+
+      lib = mkOption {
+        type = str;
+        readOnly = true;
+        default = "${cfg.root}/lib";
+      };
+
+      ensureExists = mkOption {
+        type = listOf str;
+        default = [ ];
+      };
+    };
+  };
+
+  config = {
+    dir.ensureExists = [
+      cfg.root
+      cfg.log
+      cfg.run
+      cfg.lib
+    ];
+  };
+}
