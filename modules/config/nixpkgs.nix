f429bfec759bbeca5a8ce39aa3cd9813cc3a35f5
Jason R. McNeil <jason@mcneil.dev>
2021-11-23 09:05:55 -0700

    nixpkgs: Allow "unfree"

--- modules/config/nixpkgs.nix
+++ modules/config/nixpkgs.nix
@@ -1,6 +1,10 @@
 { system, nixpkgs, nixpkgs-staging, ... }:
 {
   _module.args = {
-    pkgs = import nixpkgs { inherit system; overlays = [ nixpkgs-staging.overlay ]; };
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ nixpkgs-staging.overlay ];
+      config.allowUnfree = true;
+    };
   };
 }
