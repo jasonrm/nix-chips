49790b5be37aa63e55aa3c6b857316648d76b0cf
Jason R. McNeil <jason@mcneil.dev>
2023-05-14 01:19:44 -0700

    systemd: create directories required by tmpfiles if they are inside the configured data directory

--- modules/nixos-shims/systemd/tmpfiles.nix
+++ modules/nixos-shims/systemd/tmpfiles.nix
@@ -1,12 +1,32 @@
 {
+  config,
   pkgs,
   lib,
   modulesPath,
   ...
-}: let
-  systemdTmpfiles = pkgs.callPackage "${pkgs.path}/nixos/modules/system/boot/systemd/tmpfiles.nix" {
-    utils = pkgs.callPackage "${pkgs.path}/nixos/lib/utils.nix" {};
+}:
+with lib; let
+  inherit (pkgs) path callPackage writeText;
+  systemdTmpfiles = callPackage "${path}/nixos/modules/system/boot/systemd/tmpfiles.nix" {
+    utils = callPackage "${path}/nixos/lib/utils.nix" {};
   };
+  tmpFilesRules = writeText "tmpfiles.conf" (concatStringsSep "\n" config.systemd.tmpfiles.rules);
 in {
   options = systemdTmpfiles.options;
+  config = {
+    chips.devShell.shellHooks = ''
+      while IFS= read -r line; do
+          case "$line" in
+              d*|D*)
+                  dir_path=$(echo $line | cut -d' ' -f2)
+                  if [[ "$dir_path" == "${config.dir.data}"*  ]]; then
+                      mkdir -p "$dir_path"
+                  else
+                      "Not creating '$dir_path' as it is outside of the data directory."
+                  fi
+                  ;;
+          esac
+      done < "${tmpFilesRules}"
+    '';
+  };
 }
