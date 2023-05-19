fb91a020827526cf00e08cdfda305c2c258c3311
Jason R. McNeil <jason@mcneil.dev>
2023-05-19 00:21:00 -0700

    arcanum refinements

--- modules/chips/config/arcanum.nix
+++ modules/chips/config/arcanum.nix
@@ -8,18 +8,6 @@ with lib; let
   cfg = config.arcanum;
 in {
   config = {
-    arcanum = {
-      adminRecipients = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ3k6zKT97l8vlxcmH5hekHEvnSDXpL6j8FFW/ZL3CXT jasonrm@raskin"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII5gspOWcPeO/Qol7NbmvKIN8iQtGBYqhtPWwJMLSpYo jasonrm@elon"
-      ];
-      files = {
-        testing = {
-          source = "secrets/testing.age";
-          dest = "/dev/null";
-        };
-      };
-    };
     devShell.contents = [
       pkgs.arcanum
     ];
