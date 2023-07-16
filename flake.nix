00c60d1c0c128d38b29970db35ba3ebd63595168
Jason R. McNeil <jason@mcneil.dev>
2023-07-16 01:21:39 -0700

    flakes: update to 23.05 by default

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "dev.mcneil.nix.nix-chips";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-staging.url = "github:jasonrm/nixpkgs-staging";
 
     utils.url = "github:numtide/flake-utils";
