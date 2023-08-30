fc56602defa77eec7816d31ee852cb42c15a789a
Jason R. McNeil <jason@mcneil.dev>
2023-08-29 22:31:33 -0700

    arcanum: mkdir if it doesn't exist

--- modules/chips/services/arcanum.nix
+++ modules/chips/services/arcanum.nix
@@ -13,6 +13,12 @@ with lib; let
       echo "Encrypted Secret Not Found: $1" >&2
       exit 1
     fi
+
+    DECRYPTED_DIR=$(dirname "$2")
+    if [[ ! -d "$DECRYPTED_DIR" ]]; then
+      mkdir -p "$DECRYPTED_DIR"
+    fi
+
     ${pkgs.rage}/bin/rage -d -i ${cfg.identity} -o "$2" "$1" \
       && chmod 600 "$2"
   '';
