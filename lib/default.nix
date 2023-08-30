07881b8752392ab737a3ae35a2bc00b8fdc234e2
Jason R. McNeil <jason@mcneil.dev>
2023-08-29 23:09:03 -0700

    lib: expose traefik

--- lib/default.nix
+++ lib/default.nix
@@ -8,4 +8,5 @@ with nixpkgs.lib; {
   mergeOverlays = overlays: final: prev: (foldl' (p: next: p // (next final p)) prev overlays);
   secrets = import ./secrets.nix {inherit pkgs lib;};
   use = import ./use.nix inputs;
+  traefik = import ./traefik.nix {inherit (nixpkgs) lib;};
 }
