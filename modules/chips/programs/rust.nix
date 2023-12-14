45fbf887f58328265dafdb7067d7a2992d11bba8
Jason R. McNeil <jason@mcneil.dev>
2023-12-14 01:24:29 -0700

    rust: fix toolchain, export RUST_SRC_PATH

--- modules/chips/programs/rust.nix
+++ modules/chips/programs/rust.nix
@@ -11,15 +11,36 @@ in {
   options = with lib.types; {
     programs.rust = {
       enable = mkEnableOption "rust support";
+
+      toolchain = mkOption {
+        type = package;
+        default = pkgs.symlinkJoin {
+          name = "rust-toolchain";
+          paths = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.cargo-sort
+            pkgs.rustfmt
+            pkgs.cargo-watch
+            pkgs.rust-analyzer
+            pkgs.rustPlatform.rustcSrc
+          ];
+        };
+        description = "The rust toolchain to use";
+      };
     };
   };
 
   config = {
     devShell = mkIf cfg.enable {
+      nativeBuildInputs = [
+        cfg.toolchain
+      ];
       contents = [
-        pkgs.cargo
-        pkgs.cargo-sort
-        pkgs.rustfmt
+        pkgs.libiconv
+      ];
+      environment = [
+        "RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}"
       ];
     };
   };
