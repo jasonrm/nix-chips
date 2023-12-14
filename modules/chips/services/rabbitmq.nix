78166d824e945253f8cb39e09baeeec16af1152d
Jason R. McNeil <jason@mcneil.dev>
2023-12-13 21:08:52 -0700

    rabbitmq: enable traefik endpoint if rabbitmq and the managementPlugin is enabled

---
+++ modules/chips/services/rabbitmq.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  cfg = config.services.rabbitmq;
+in {
+  config = lib.mkMerge [
+    (lib.mkIf (cfg.enable && cfg.managementPlugin.enable) {
+      services.traefik = {
+        routers = {
+          rabbitmq = {
+            service = "rabbitmq";
+            rule = "Host(`rabbitmq.localhost`)";
+          };
+        };
+        services = {
+          rabbitmq = {
+            loadBalancer.servers = [
+              {url = "http://127.0.0.1:${toString cfg.managementPlugin.port}";}
+            ];
+          };
+        };
+      };
+    })
+  ];
+}
