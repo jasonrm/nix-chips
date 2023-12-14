7bd611aae775d660592050f44609261f7261bf6f
Jason R. McNeil <jason@mcneil.dev>
2023-12-14 01:23:42 -0700

    template: rough example of how to pull in additional flakes and use overlays

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -3,15 +3,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-staging.url = "github:jasonrm/nixpkgs-staging";
 
-    chips.url = "github:jasonrm/nix-chips";
-    chips.inputs.nixpkgs.follows = "nixpkgs";
-    chips.inputs.nixpkgs-staging.follows = "nixpkgs-staging";
+    chips = {
+      url = "github:jasonrm/nix-chips";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-staging.follows = "nixpkgs-staging";
+    };
+
+    # flake-utils = {
+    #   url = "github:numtide/flake-utils";
+    # };
+
+    # rust-overlay = {
+    #   url = "github:oxalica/rust-overlay";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.flake-utils.follows = "flake-utils";
+    # };
   };
 
   outputs = {
     self,
     nixpkgs,
     chips,
+    # rust-overlay,
     ...
   }:
     chips.lib.use {
@@ -19,5 +32,8 @@
       # packagesDir = ./nix/packages;
       # nixosModulesDir = ./nix/nixosModules;
       # dockerImagesDir = ./nix/dockerImages;
+      overlays = [
+        # rust-overlay.overlays.default
+      ];
     };
 }
