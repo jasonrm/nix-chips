90a4d7f5f28584f17d99cd4d85eb7dc834cfcc27
Jason R. McNeil <jason@mcneil.dev>
2024-01-09 15:52:21 -0700

    home-manager: check for identity before using, print progress

--- modules/home-manager/arcanum.nix
+++ modules/home-manager/arcanum.nix
@@ -9,11 +9,17 @@ with lib; let
 
   decryptSecret = pkgs.writeShellScript "decrypt" ''
     set -o errexit -o nounset -o pipefail
+    if [ ! -f ${cfg.identity} ]; then
+      echo "Missing identity file: ${cfg.identity}" >&2
+      exit 0
+    fi
+
     if [ ! -f "$1" ]; then
       echo "Encrypted Secret Not Found: $1" >&2
       exit 0
     fi
 
+    echo "Decrypting: $1 -> $2"
     DECRYPTED_DIR=$(dirname "$2")
     if [[ ! -d "$DECRYPTED_DIR" ]]; then
       mkdir -p "$DECRYPTED_DIR"
