b623eaf7e417fa0d2c734ac73171ab665e6ee171
Jason R. McNeil <jason@mcneil.dev>
2024-02-11 01:03:54 -0700

    taskfile: add program with defaults

---
+++ modules/chips/programs/taskfile.nix
@@ -0,0 +1,138 @@
+{
+  system,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.programs.taskfile;
+
+  taskfileConfigFile = pkgs.writeText "Taskfile.yml" (builtins.toJSON cfg.config);
+
+  taskSourceSubmodule = with types; {
+    options = {
+      exclude = mkOption {
+        type = str;
+      };
+    };
+  };
+
+  taskCommandSubmodule = with types; {
+    options = {
+      cmd = mkOption {
+        type = str;
+      };
+      for = mkOption {
+        type = nullOr (oneOf [attrs str]);
+        default = null;
+      };
+    };
+  };
+
+  taskPreconditionSubmodule = with types; {
+    options = {
+      sh = mkOption {
+        type = str;
+      };
+      msg = mkOption {
+        type = str;
+      };
+    };
+  };
+
+  taskSubmodule = with types; {
+    options = {
+      cmds = mkOption {
+        type = listOf (oneOf [str (submodule taskCommandSubmodule)]);
+        default = [];
+      };
+      preconditions = mkOption {
+        type = listOf (oneOf [str (submodule taskPreconditionSubmodule)]);
+        default = [];
+      };
+      deps = mkOption {
+        type = nullOr (listOf str);
+        default = null;
+      };
+      generates = mkOption {
+        type = nullOr (listOf str);
+        default = null;
+      };
+      sources = mkOption {
+        type = listOf (oneOf [str (submodule taskSourceSubmodule)]);
+        default = [];
+      };
+      desc = mkOption {
+        type = str;
+      };
+    };
+  };
+
+  taskfileConfig = with types; {
+    options = {
+      tasks = mkOption {
+        type = attrsOf (submodule taskSubmodule);
+        default = {};
+      };
+      version = mkOption {
+        type = str;
+        default = "3";
+      };
+    };
+  };
+in {
+  options = {
+    programs.taskfile = with types; {
+      enable = mkEnableOption "taskfile support";
+      config = mkOption {
+        type = submodule taskfileConfig;
+        default = {};
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.taskfile.config.tasks = {
+      update-nix = {
+        desc = "Update Nix Flakes";
+        cmds = ["${pkgs.nix}/bin/nix flake update"];
+      };
+
+      format-nix = {
+        desc = "Format Nix Files";
+        cmds = [''${pkgs.alejandra}/bin/alejandra {{.CLI_ARGS | default "." }}''];
+      };
+      format-json = {
+        desc = "Format JSON Files";
+        cmds = [''for FILE in {{.CLI_ARGS | default "*.json" }}; do ${pkgs.jq}/bin/jq --indent 2 . "$FILE" > "$FILE".tmp && mv "$FILE".tmp "$FILE"; done''];
+      };
+
+      check = {
+        desc = "Run All Check Tasks";
+      };
+      format = {
+        desc = "Run All Format Tasks";
+        deps = ["format-nix" "format-json"];
+      };
+      install = {
+        desc = "Run All Install Tasks";
+      };
+      update = {
+        desc = "Run All Update Tasks";
+        deps = ["update-nix"];
+      };
+      build = {
+        desc = "Run All Build Tasks";
+      };
+    };
+    devShell = {
+      contents = [
+        pkgs.go-task
+      ];
+      shellHooks = ''
+        ln -sf ${taskfileConfigFile} Taskfile.yml
+      '';
+    };
+  };
+}
