728b033b7edfa333375f51b22106b435dd9e0856
Jason R. McNeil <jason@mcneil.dev>
2024-08-15 23:14:06 -0700

    elasticmq: Init with basic config

---
+++ modules/chips/services/elasticmq.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  cfg = config.services.elasticmq;
+in {
+  options = {
+    services.elasticmq = with lib.types; {
+      enable = lib.mkEnableOption "Enable elasticmq.";
+      configFile = lib.mkOption {
+        type = nullOr path;
+      };
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      devShell.environment = [
+        "ELASTICMQ_BASE_URL=http://127.0.0.1:9324/queue/"
+      ];
+
+      programs.supervisord.programs = {
+        elasticmq = {
+          command = "${pkgs.elasticmq-server-bin}/bin/elasticmq-server";
+          environment =
+            if cfg.configFile != null
+            then [
+              "JAVA_TOOL_OPTIONS=-Dconfig.file=${cfg.configFile}"
+            ]
+            else [];
+        };
+      };
+    })
+  ];
+}
