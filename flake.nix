e20dddc037137812140c9cd19dfd5ab541cc25b0
Jason R. McNeil <jason@mcneil.dev>
2025-05-06 01:15:08 -0700

    Try follow on inputs again

--- flake.nix
+++ flake.nix
@@ -5,17 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     utils.url = "github:numtide/flake-utils";
     nixpkgs-staging.url = "github:jasonrm/nixpkgs-staging";
+    nixpkgs-staging.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-staging,
-    home-manager,
-    utils,
-    ...
-  } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-staging,
+      home-manager,
+      utils,
+      ...
+    }@inputs:
     {
       lib = import ./lib inputs;
     }
