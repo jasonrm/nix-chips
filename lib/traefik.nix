8cb0b679a7999ffa14001b03fd447fa88e4e8ada
Jason R. McNeil <jason@mcneil.dev>
2025-09-13 20:16:06 -0700

    minor cleanup

--- lib/traefik.nix
+++ lib/traefik.nix
@@ -3,9 +3,6 @@
     (lib)
     concatStringsSep
     flatten
-    optionals
-    assertMsg
-    elemAt
     ;
   withDomains = prefixes: domains: flatten (map (d: (map (p: ''HostRegexp(`${p}.${d}`)'') prefixes)) domains);
   hostRegexp = prefixes: domains: concatStringsSep "||" (withDomains prefixes domains);
