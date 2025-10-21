04f3c5ba2422e87e36647db2ced03fcab7df340e
Jason R. McNeil <jason@mcneil.dev>
2025-10-20 22:38:00 -0700

    mysql: note why path override

--- modules/nixos-shims/services/mysql.nix
+++ modules/nixos-shims/services/mysql.nix
@@ -13,6 +13,7 @@ with lib; {
       dataDir = mkForce (config.dir.data + "/mysql");
     };
     systemd.services.mysql = {
+      # work around https://github.com/NixOS/nixpkgs/blob/5e2a59a5b1a82f89f2c7e598302a9cacebb72a67/nixos/modules/services/databases/mysql.nix#L526
       path = mkForce [];
     };
   };
