409bf213afc3bdde9e0522e8972187aef5e8b3be
Jason R. McNeil <jason@mcneil.dev>
2025-11-18 23:21:49 -0700

    Only override path if mysql is enabled

--- modules/nixos-shims/services/mysql.nix
+++ modules/nixos-shims/services/mysql.nix
@@ -12,7 +12,7 @@ with lib; {
       package = mkDefault pkgs.mysql84;
       dataDir = mkForce (config.dir.data + "/mysql");
     };
-    systemd.services.mysql = {
+    systemd.services.mysql = mkIf config.services.mysql.enable {
       # work around https://github.com/NixOS/nixpkgs/blob/5e2a59a5b1a82f89f2c7e598302a9cacebb72a67/nixos/modules/services/databases/mysql.nix#L526
       path = mkForce [];
     };
