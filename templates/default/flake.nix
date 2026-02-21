179c7868b764e58735ebd979f4b6b193526cdddf
Jason R. McNeil <jason@mcneil.dev>
2026-02-21 11:42:57 -0700

    template: minor tweaks

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # various, usually obscure, programs that are missing from nixpkgs
     nixpkgs-staging.url = "github:jasonrm/nixpkgs-staging";
 
