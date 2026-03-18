859393ff3db47aa088e38e0e05a90129a4079bca
Jason R. McNeil <jason@mcneil.dev>
2026-03-18 14:44:49 -0700

    taskfile: replace strongly-typed submodules with freeform attrs

--- modules/chips/programs/taskfile.nix
+++ modules/chips/programs/taskfile.nix
@@ -8,101 +8,37 @@
 with lib; let
   cfg = config.programs.taskfile;
 
+  jsonFormat = pkgs.formats.json {};
+
+  taskSubmodule = types.submodule {
+    freeformType = jsonFormat.type;
+    options.deps = mkOption {
+      type = types.listOf types.str;
+      default = [];
+    };
+  };
+
   taskfileConfigFile = pkgs.writeText "Taskfile.yml" (builtins.toJSON cfg.config);
-
-  taskSourceSubmodule = with types; {
-    options = {
-      exclude = mkOption {type = str;};
-    };
-  };
-
-  taskCommandSubmodule = with types; {
-    options = {
-      cmd = mkOption {type = str;};
-      for = mkOption {
-        type = nullOr (oneOf [
-          attrs
-          str
-        ]);
-        default = null;
-      };
-    };
-  };
-
-  taskPreconditionSubmodule = with types; {
-    options = {
-      sh = mkOption {type = str;};
-      msg = mkOption {type = str;};
-    };
-  };
-
-  taskSubmodule = with types; {
-    options = {
-      cmds = mkOption {
-        type = listOf (oneOf [
-          str
-          (submodule taskCommandSubmodule)
-        ]);
-        default = [];
-      };
-      preconditions = mkOption {
-        type = listOf (oneOf [
-          str
-          (submodule taskPreconditionSubmodule)
-        ]);
-        default = [];
-      };
-      deps = mkOption {
-        type = nullOr (listOf str);
-        default = null;
-      };
-      generates = mkOption {
-        type = nullOr (listOf str);
-        default = null;
-      };
-      env = mkOption {
-        type = attrsOf str;
-        default = {};
-      };
-      sources = mkOption {
-        type = listOf (oneOf [
-          str
-          (submodule taskSourceSubmodule)
-        ]);
-        default = [];
-      };
-      desc = mkOption {type = str;};
-      dir = mkOption {
-        type = nullOr str;
-        default = null;
-      };
-    };
-  };
-
-  taskfileConfig = with types; {
-    options = {
-      tasks = mkOption {
-        type = attrsOf (submodule taskSubmodule);
-        default = {};
-      };
-      version = mkOption {
-        type = str;
-        default = "3";
-      };
-    };
-  };
 in {
   options = {
     programs.taskfile = with types; {
       enable = mkEnableOption "taskfile support";
       config = mkOption {
-        type = submodule taskfileConfig;
+        type = submodule {
+          freeformType = jsonFormat.type;
+          options.tasks = mkOption {
+            type = attrsOf taskSubmodule;
+            default = {};
+          };
+        };
         default = {};
+        description = "Freeform Taskfile configuration. Supports any valid Taskfile YAML structure as a Nix attrset.";
       };
     };
   };
 
   config = mkIf cfg.enable {
+    programs.taskfile.config.version = "3";
     programs.taskfile.config.tasks = {
       update-nix = {
         desc = "Update Nix Flakes";
