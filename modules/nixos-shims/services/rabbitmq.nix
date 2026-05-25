bcbbcd0d57f384142588960bf6fe2e7822dac37b
Jason R. McNeil <jason@mcneil.dev>
2026-05-25 06:51:14 -0700

    rabbitmq: set management_agent.disable_metrics_collector

--- modules/nixos-shims/services/rabbitmq.nix
+++ modules/nixos-shims/services/rabbitmq.nix
@@ -11,6 +11,9 @@ with lib; {
     services.rabbitmq = {
       dataDir = mkForce (config.dir.data + "/rabbitmq");
       managementPlugin.enable = mkDefault true;
+      configItems = mkIf config.services.rabbitmq.managementPlugin.enable {
+        "management_agent.disable_metrics_collector" = mkDefault "true";
+      };
     };
   };
 }
